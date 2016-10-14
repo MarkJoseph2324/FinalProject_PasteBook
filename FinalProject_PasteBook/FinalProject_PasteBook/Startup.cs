b600b8f7f7dd7f78bd7c63f88db3a54d26bbe547
Bootcamp <Bootcamp@PICCPU-0726>
2016-10-14 15:32:56 +0800

    master

---
+++ FinalProject_PasteBook/FinalProject_PasteBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProject_PasteBook.Startup))]
+namespace FinalProject_PasteBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
