950a506acea310a07a8b09f5f3db2f7cc7f79d0b
Bootcamp <Bootcamp@PICCPU-0726>
2016-10-17 08:18:52 +0800

    password manager

---
+++ FinalProject_PasteBook/BusinessLogic/Managers/PasswordManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class PasswordManager
+    {
+        private static RNGCryptoServiceProvider CryptoServiceProvider = null;
+        private const int SaltSize = 24;
+
+        public string GeneratePasswordHash(string plainTextPassword, out string salt)
+        {
+            salt = GetSaltString();
+            string finalString = plainTextPassword + salt;
+            return GetPasswordHashAndSalt(finalString);
+        }
+
+        public bool IsPasswordMatch(string password, string salt, string hash)
+        {
+            string finalString = password + salt;
+            return hash == GetPasswordHashAndSalt(finalString);
+        }
+
+        public string GetPasswordHashAndSalt(string message)
+        {
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] dataBytes = GetBytes(message);
+            byte[] resultBytes = sha.ComputeHash(dataBytes);
+            return GetString(resultBytes);
+        }
+
+        public byte[] GetBytes(string message)
+        {
+            return Encoding.ASCII.GetBytes(message);
+        }
+
+        public string GetString(byte[] resultBytes)
+        {
+            return Encoding.ASCII.GetString(resultBytes);
+        }
+        
+        public void SaltGenerator()
+        {
+            CryptoServiceProvider = new RNGCryptoServiceProvider();
+        }
+
+        public string GetSaltString()
+        {
+            byte[] saltBytes = new byte[SaltSize];
+            CryptoServiceProvider.GetNonZeroBytes(saltBytes);
+            string saltString = GetString(saltBytes);
+            return saltString;
+        }
+    }
+}
